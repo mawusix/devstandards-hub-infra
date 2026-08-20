aa5820334ea591f607b995c5b52b80f2232e6b16
mawusix <ameteweem@gmail.com>
2026-08-20 21:50:22 +0100

    feat: Add IAM member for artifact registry access and project data retrieval

--- modules/artifact-registry/main.tf
+++ modules/artifact-registry/main.tf
@@ -35,3 +35,15 @@ resource "google_artifact_registry_repository" "docker" {
     }
   }
 }
+
+data "google_project" "this" {
+  project_id = var.project_id
+}
+
+resource "google_artifact_registry_repository_iam_member" "node_puller" {
+  project    = var.project_id
+  location   = var.region
+  repository = google_artifact_registry_repository.this.name
+  role       = "roles/artifactregistry.reader"
+  member     = "serviceAccount:${data.google_project.this.number}-compute@developer.gserviceaccount.com"
+}
